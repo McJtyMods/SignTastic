89683161bda5168a4a09186ba86b6025e30493df
Jorrit Tyberghein <jorrit.tyberghein@nexuzhealth.com>
2025-11-13 18:58:13 +0100

    Various cleanups

--- src/main/java/com/mcjty/signtastic/setup/Registration.java
+++ src/main/java/com/mcjty/signtastic/setup/Registration.java
@@ -35,7 +35,7 @@ public class Registration {
     public static final DeferredRegister<MenuType<?>> CONTAINERS = DeferredRegister.create(Registries.MENU, MODID);
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
     public static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES = DeferredRegister.create(NeoForgeRegistries.Keys.ATTACHMENT_TYPES, MODID);
-    public static final DeferredRegister.DataComponents COMPONENTS = DeferredRegister.createDataComponents(MODID);
+    public static final DeferredRegister.DataComponents COMPONENTS = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, MODID);
 
     public static void register(IEventBus bus) {
         RBLOCKS.register(bus);
